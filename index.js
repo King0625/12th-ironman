b78ca14c2ae3823be2f4ec9bdc3da4ae56118206
kenchen <independentchen@gmail.com>
2020-08-26 16:57:15 +0800

    Feat: init project

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT | 3000;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.json({
+    result: "Hello"
+  });
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
