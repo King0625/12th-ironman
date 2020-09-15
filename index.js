d173312a11ea65fc5958965bdf0cff50a112f215
kenchen <independentchen@gmail.com>
2020-09-15 10:04:23 +0800

    Feat: add throttle and indexRouter

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT | 3000;
+const throttle = require('./middleware/throttle');
+const indexRouter = require('./routes/index');
 
+app.use(throttle(60));
 app.use(cors());
-
-app.get('/', (req, res) => {
-  res.json({
-    result: "Hello"
-  });
-})
+app.use('/', indexRouter);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
