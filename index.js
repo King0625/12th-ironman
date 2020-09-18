56e60ce57e9b4f956fc2bd77829db74c82fb1c86
kenchen <independentchen@gmail.com>
2020-09-18 18:11:27 +0800

    Feat: add morgan logger

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 require('./tasks/warning');
 const express = require('express');
 const cors = require('cors');
+const logger = require('morgan');
 const app = express();
 const port = process.env.PORT | 3000;
 const throttle = require('./middleware/throttle');
 const indexRouter = require('./routes/index');
 
+app.use(logger('dev'));
 app.use(throttle(60));
 app.use(cors());
 app.use('/', indexRouter);
